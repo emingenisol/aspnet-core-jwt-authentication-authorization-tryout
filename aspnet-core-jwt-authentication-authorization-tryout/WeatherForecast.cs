6ef549543e52fa0d2c9c5dcb0d96166eb53f5ff1
musta <musta@KERTELPANDEHIT>
2023-06-04 02:03:03 +0300

    Add project files.

---
+++ aspnet-core-jwt-authentication-authorization-tryout/WeatherForecast.cs
@@ -0,0 +1,13 @@
+namespace aspnet_core_jwt_authentication_authorization_tryout
+{
+    public class WeatherForecast
+    {
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+    }
+}
